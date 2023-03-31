6c1858b98f2ae84c3fe95443e8f894cdd5d1d6f2
Lezlie Espana <lespana@mcw.edu>
2023-03-31 14:50:01 -0500

    Adapting Mayer's outlier script to python

---
+++ src/meierlab/outliers.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+import numpy as np
+import pandas as pd
+from nilearn.image import mean_img, math_img
+from nipype.utils.filemanip import split_filename
+from nibabel import save, load, Nifti1Image
+from pathlib import Path
+from meierlab.masks import create_avg_mask
+
+
+# TODO
+# generate_graphs(): plotly interactive dashboard/images
+
+
+def compute_sd_thresholds(mean, sd, threshold=3):
+    above_str = f"mean_img + {threshold} * sd_img"
+    above = math_img(above_str,mean_img=mean,sd_img=sd)
+    below_str = f"mean_img - {threshold} * sd_img"
+    below = math_img(below_str,mean_img=mean,sd_img=sd)
+    thresh_above = Nifti1Image(
+        np.full_like(mean.get_fdata(),200),affine=mean.affine
+    )
+    thresh_below = Nifti1Image(
+        np.full_like(mean.get_fdata(),-200),affine=mean.affine
+    )
+
+    return above, below, thresh_above, thresh_below
+
+def compute_outlier_nii(nii_img,mask,mean,sd,
+                        above,below,thresh_above,thresh_below):
+    outlier_nii = math_img(
+        '(-1 * (np.less_equal(bthresh_img,img) & np.less_equal(img,below_img))) + (1*(np.less_equal(above_img,img) & np.less_equal(img,athresh_img)))',
+        img=nii_img,mean_img=mean,mask_img=mask,sd_img=sd,
+        below_img=below,above_img=above,
+        athresh_img=thresh_above,bthresh_img=thresh_below
+    )
+
+    return outlier_nii
+
+def compute_outlier_values(in_folder, save_nii=False, mask=None):
+    out_df = pd.DataFrame(
+        columns=["subject","total_voxels","pos_count",
+                 "pos_pct","neg_count","neg_pct"]
+    )
+
+    nii_images = []
+    for img in list(Path(in_folder).glob("*nii.gz")):
+        nii_images.append(img)
+    
+    if not mask:
+        mask_nii = create_avg_mask(nii_images)
+
+    mask_nii = load(mask)
+    total_voxels = np.count_nonzero(mask_nii.get_fdata())
+
+    mean_nii = mean_img(nii_images)
+    sd_nii = math_img("np.std(img,axis=-1)",img=nii_images)
+    above, below, thresh_above, thresh_below = compute_sd_thresholds(mean_nii,sd_nii)
+    
+    for nii_img in nii_images:
+        _, subject, _ = split_filename(nii_img)
+        outlier_nii = compute_outlier_nii(nii_img,mask_nii,
+                                          mean_nii,sd_nii,above,below,thresh_above,thresh_below
+                                          )
+        outlier_masked = math_img('mask_img * img',mask_img=mask_nii,img=outlier_nii)
+        if save_nii:
+            save(outlier_masked, in_folder / f"{subject}_outliers")
+
+        pos_count = np.count_nonzero(outlier_masked.get_fdata() > 0)
+        neg_count = np.count_nonzero(outlier_masked.get_fdata() < 0)
+        pos_pct = np.round(pos_count / total_voxels,5)
+        neg_pct = np.round(neg_count / total_voxels,5)
+
+        subj_out = pd.DataFrame([{'subject':subject,'total_voxels':total_voxels,
+                    'pos_count':pos_count,'pos_pct':pos_pct,
+                    'neg_count':neg_count,'neg_pct':neg_pct
+                    }])
+        
+        out_df = pd.concat([out_df,subj_out], ignore_index=True)
+
+    return out_df
