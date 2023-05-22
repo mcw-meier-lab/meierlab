579caf98aa15d821acb08adbd6b2e92480230e1c
Lezlie Espana <lespana@mcw.edu>
2023-05-22 11:34:21 -0500

    fixing docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,38 +1,104 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import re
+import sys
+import sphinx
+
+sys.path.insert(0, os.path.abspath("sphinxext"))
+from github_link import make_linkcode_resolve
+
+# We also add the directory just above to enable local imports
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = u"meierlab"
-copyright = u"2023, Lezlie Espana"
-author = u"Lezlie Espana"
+project = 'meierlab'
+copyright = '2023, MCW-Meier-Lab'
+author = 'MCW Meier Lab team'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    "myst_nb",
-    "autoapi.extension",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.coverage",
+    "sphinx.ext.linkcode",
+    "sphinx_design",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.todo",
+    "sphinx_gallery.gen_gallery",
+    "sphinx_copybutton",
+    "myst_parser",
+    "numpydoc",
 ]
-autoapi_dirs = ["../src"]
-autoapi_type = 'python'
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+autosummary_generate = True
+autodoc_default_options = {
+    "imported-members": True,
+    "inherited-members": True,
+    "undoc-members": True,
+    "member-order": "bysource",
+}
+
+numpydoc_show_class_members = False
+
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3.10/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    "nibabel": ("https://nipy.org/nibabel", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "nilearn": ("https://nilearn.github.io/stable/", None)
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+source_suffix = [".rst", ".md"]
+
+plot_gallery = True
+
+# Latest release version
+latest_release = re.match(
+    r"v?([0-9]+.[0-9]+.[0-9]+).*",
+    os.popen("git describe --tags").read().strip(),
+).groups()[0]
+
+# The full current version, including alpha/beta/rc tags.
+import meierlab
+
+current_version = meierlab.__version__
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_rtd_theme"
-source_suffix = ['.md']
+html_theme = 'furo'
+html_static_path = ['_static']
+html_title = "meierlab"
+html_short_title = "meierlab"
+
+copybutton_prompt_text = ">>> "
+
+sphinx_gallery_conf = {
+    "doc_module": "meierlab",
+    "reference_url": {"meierlab": None},
+    "examples_dirs": "../examples/",
+    "gallery_dirs": "auto_examples",
+}
+
+# The following is used by sphinx.ext.linkcode to provide links to github
+linkcode_resolve = make_linkcode_resolve(
+    "meierlab",
+    "https://github.com/mcw-meier-lab/meierlab",
+    "meierlab/blob/{revision}/"
+    "{package}/{path}#L{lineno}",
+)
---
+++ docs/index.rst
@@ -0,0 +1,72 @@
+.. meierlab documentation master file, created by
+   sphinx-quickstart on Fri May 19 13:27:07 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to the MCW Meier Lab documentation!
+===========================================
+
+.. container:: index-paragraph
+   The **meierlab** project provides functional tools to aid 
+   lab members in downloading, processing, visualizing, 
+   and analyzing imaging data.
+
+
+.. grid::
+
+   .. grid-item-card:: :fas: `rocket` Quickstart
+      :link: quickstart
+      :link-type: ref
+      :columns: 12 12 4 4
+      :class-card: sd-shadow-md
+      :class-title: sd-text-primary
+      :margin: 2 2 0 0
+
+      Get started with meierlab.
+
+
+   .. grid-item-card:: :fas:`th` Examples
+      :link: auto_examples/index.html
+      :link-type: url
+      :columns: 12 12 4 4
+      :class-card: sd-shadow-md
+      :class-title: sd-text-primary
+      :margin: 2 2 0 0
+
+      Discover functionalities by reading examples.
+
+
+   .. grid-item-card:: :fas:`book` User guide
+      :link: user_guide
+      :link-type: ref
+      :columns: 12 12 4 4
+      :class-card: sd-shadow-md
+      :class-title: sd-text-primary
+      :margin: 2 2 0 0
+
+      Learn about the tools provided.
+
+Featured examples
+-----------------
+**In progress**
+
+.. toctree::
+   :hidden:
+   :includehidden:
+   :titlesonly:
+
+   auto_examples/index.rst
+   user_guide.rst
+   modules/index.rst
+   glossary.rst
+
+.. toctree::
+   :hidden:
+   :caption: Development
+
+   changes/whats_new.rst
+   authors.rst
+   Github Repository <https://github.com/mcw-meier-lab/meierlab>
+
+
+The meierlab project is developed and used for academic research only.
