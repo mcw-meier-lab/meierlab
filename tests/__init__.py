4847ebc7f8fde5b03a146d66f8fb182546d400e4
Lezlie Espana <lespana@mcw.edu>
2023-12-08 14:46:17 -0600

    environment fix

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,5 +6,5 @@ path = Path(meierlab.__file__).parent.parent.parent.absolute()
 ATLAS_FILE= path / "tests/data/atlas.csv"
 SUB_FILE= path / "tests/data/sub-atlas.tsv"
 
-os.environ["ATLAS_FILE"] = ATLAS_FILE
-os.environ["SUB_FILE"] = SUB_FILE
+os.environ["ATLAS_FILE"] = str(ATLAS_FILE)
+os.environ["SUB_FILE"] = str(SUB_FILE)
